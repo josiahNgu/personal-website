0daa775015dca05d9325cdfcf96f06df77b42167
Josiah Ngu <jos81498@gmail.com>
2018-09-28 13:54:19 -0500

    initiial commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import logo from './misc/logo.png';
+import './style/App.css';
+import Header from './components/Header.js';
 class App extends Component {
   render() {
     return (
@@ -13,6 +13,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Header/>
       </div>
     );
   }
---
+++ my-app/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+ class Header extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <p>
+          this is a test
+        </p>
+      </div>
+    );
+  }
+}
+export default Header;
