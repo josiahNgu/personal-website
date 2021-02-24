a87f09eb449848ef7131f9115fdb948b9426e1a5
josiahNgu <josiahngu@gmail.com>
2021-02-23 22:42:04 -0500

    added three.js 3d graphic on landing page

--- my-app/src/components/LandingPage/LandingPage.js
+++ my-app/src/components/LandingPage/LandingPage.js
@@ -1,25 +1,74 @@
-import React from "react";
+import React, { Suspense, useRef, useEffect } from "react";
 import "./LandingPage.scss";
-import SidePic from "../../misc/landing_page.svg";
+import { Html } from "@react-three/drei/";
+import { Section } from "../section";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Canvas, useLoader, useFrame } from "react-three-fiber";
+// drei is useful plugins to render shapes so you don't have to create it yourself
+import state from "../state";
+const Model = ({ modelPath }) => {
+  const gltf = useLoader(GLTFLoader, modelPath);
+  return <primitive object={gltf.scene} dispose={null} position={[0, 0, 0]} />;
+};
+const Lights = () => {
+  return (
+    <>
+      <ambientLight intensity={0.3} />
+      <directionalLight position={[10, 10, 5]} intensity={1} />
+      <directionalLight position={[0, 10, 0]} intensity={1.5} />
+      <spotLight intensity={1} position={[1000, 0, 0]} />
+    </>
+  );
+};
+const HTMLContent = ({ domContent, children, modelPath, positionY }) => {
+  const ref = useRef();
+  useFrame(() => (ref.current.rotation.y += 0.003));
 
-import DownArrow from "../../misc/down_arrow.svg";
+  return (
+    // offset is the force of how much it move when you scroll
+    <Section factor={1} offset={1}>
+      <group position={[0, positionY, 0]}>
+        <mesh ref={ref} position={[-40, 100, -250]}>
+          <Model modelPath={modelPath} />
+        </mesh>
+        <Html portal={domContent} fullscreen>
+          {children}
+        </Html>
+      </group>
+    </Section>
+  );
+};
+
 const LandingPage = () => {
+  const domContent = useRef();
+  // const scrollArea = useRef();
+  // const onScroll = (e) => (state.top.current = e.target.scrollTop);
+  // useEffect(() => void onScroll({ target: scrollArea.current }), []);
   return (
     <div className="landing-page">
-      <div className="left-side">
-        <h2 className="title">I'm Josiah Ngu</h2>
-
-        <h3 className="subheading">
-          Currently Maryland based full stack developer who specialized in web
-          development. Passionate about turning problems into solutions and the
-          power of computing. Always appreciate a clean and minimalistic design
-          and architectures.
-        </h3>
-      </div>
-      <div class="right-side">
-        <img className="side-img" src={SidePic} alt="code svg" />
-        <img className="view-more" src={DownArrow} alt="view more" />
-      </div>
+      <Canvas
+        colorManangent
+        // x,y,z
+        camera={{ position: [0, 0, 120], fov: 110 }}
+      >
+        <Lights />
+        <Suspense fallback={null}>
+          <HTMLContent
+            domContent={domContent}
+            modelPath="scene.gltf"
+            positionY={250}
+          >
+            <div class="container">
+              <h1 className="title">Josiah Ngu</h1>
+              <h1 className="subheading">Full Stack Software Developer</h1>
+            </div>
+          </HTMLContent>
+        </Suspense>
+      </Canvas>
+      {/* <div class="scrollArea" ref={scrollArea} onScroll={onScroll}>
+        <div style={{ position: "sticky", top: 0 }}></div>
+        <div style={{ height: `${state.sections * 100}vh` }}></div>
+      </div> */}
     </div>
   );
 };
