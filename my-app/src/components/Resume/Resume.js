f33f46f872811ce0282c4376aefe32bf1f2a695e
Josiah Ngu <josiahgnu@gmail.com>
2023-01-31 21:33:36 -0500

    Update work experience and colors

--- my-app/src/components/Resume/Resume.js
+++ my-app/src/components/Resume/Resume.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Resume.scss";
-import { BsArrowReturnRight } from 'react-icons/bs';
+import { BsArrowReturnRight } from "react-icons/bs";
 import { openLinkInNewWindow } from "../../utilities/utilities";
 const resumeLink =
   "https://drive.google.com/open?id=10HU5IXSSJLSGV_ukD_qoPgKUXwkY3geo";
@@ -16,7 +16,7 @@ const knowledges = [
   "Data & Network Security",
   "Web Application Development",
   "Operating System Kernels",
-  "Software Lifecycle & Methodologists"
+  "Software Lifecycle & Methodologists",
 ];
 
 const resume = () => {
@@ -24,14 +24,19 @@ const resume = () => {
     <div className="Resume">
       <div className=" container col-lg-10 ">
         <div>
-          <h2 className="d-inline light-slate-color ">
-            <b >/EDUCATION</b>
+          <h2 className="d-inline alternate-color">
+            <b>/EDUCATION</b>
           </h2>
           <ul className="mt-3">
-            {
-              knowledges.map((item, index) => <li className="mb-2" key={index}><BsArrowReturnRight className="mr-2 alternate-color " style={{ fontSize: '1.1rem' }} />{item}</li>)
-            }
-
+            {knowledges.map((item, index) => (
+              <li className="mb-2" key={index}>
+                <BsArrowReturnRight
+                  className="mr-2 alternate-color "
+                  style={{ fontSize: "1.1rem" }}
+                />
+                {item}
+              </li>
+            ))}
           </ul>
 
           {/* <button
@@ -42,7 +47,6 @@ const resume = () => {
           </button> */}
         </div>
       </div>
-
     </div>
   );
 };
@@ -60,6 +64,3 @@ export default resume;
           <li> Software Lifecycle & Methodologists</li>
 
           */
-
-
-
